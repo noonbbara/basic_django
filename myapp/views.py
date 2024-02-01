a006b96d5c6fec6e285126a551dcd79e766dcbe1
트북노\ysys1 <ysysys1226@gmail.com>
2024-02-01 11:25:23 +0900

    [UPDATE]

--- myapp/views.py
+++ myapp/views.py
@@ -5,27 +5,39 @@ topics = [
   {'id':2, 'title':'view', 'body':'View is ..'},
   {'id':3, 'title':'model', 'body':'Model is ..'}
 ]
-# Create your views here.
-def index(request):
+
+def HTMLTemplate(articleTag):
   global topics
   ol = ''
   for topic in topics:
-    ol += f'<li>{topic["title"]}</li>'
-  return HttpResponse(f'''
+    ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+  return f'''
   <html>
   <body>
-    <h1>Django</h1>
-    <ol>
+    <h1><a href="/">Django</a></h1>
+    <ul>
       {ol}
-    </ol>
-    <h2>Welcome</h2>
-    Hello, Django
+    </ul>
+    {articleTag}
   </body>
   </html>
-''')
+'''
+# Create your views here.
+def index(request):
+  article='''
+  <h2>Welcome</h2>
+  Hello, Django
+  '''
+  return HttpResponse(HTMLTemplate(article))
 
 def create(request):
   return HttpResponse('Create')
 
 def read(request, id):
-  return HttpResponse('Read!'+id)
+  global topics
+  article=''
+  for topic in topics:
+    if topic['id'] == int(id):
+      article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+
+  return HttpResponse(HTMLTemplate(article))
